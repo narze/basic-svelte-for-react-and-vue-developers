f583b12e8a3c0d336d66f72d513ff5b2b7c3133d
Manassarn Manoonchai <manassarn@gmail.com>
2022-06-18 17:44:18 +0700

    chore: move files

---
+++ src/components/counter/VueCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="counter">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+  <div class="counter-message">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+const count = ref(0)
+const add = () => (count.value = count.value + 1)
+const subtract = () => (count.value = count.value - 1)
+</script>
